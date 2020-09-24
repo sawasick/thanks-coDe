263e0945d066e4777c824ce8d018655320a9bd4a
sawasick <50393229+sawasick@users.noreply.github.com>
2020-09-24 19:22:34 +0900

    update
    fix member's name
    add circle animation

--- js/main.js
+++ js/main.js
@@ -34,7 +34,6 @@
 // window.onload = printTxt1(printTxt2);
 // printTxt2(printTxt3);
 
-
 // 上から順番に処理を実行する
 // Promise.resolve()
 //   .then(printTxt1) // .then(実行したい関数名)
@@ -47,7 +46,6 @@
 //       resolve();
 //     });
 //   });
-
 
 // function printTxt1(){
 //   var ok = false;
@@ -105,56 +103,64 @@
 //   });
 // }
 
+$(window).on("load", function () {
+	// ここから文字を<span></span>で囲む記述
+	for (var i = 0; i <= 11; i++) {
+		var setElm = $(".txt" + i),
+			className = "t" + i + "-";
+		setElm
+			.children()
+			.addBack()
+			.contents()
+			.each(function () {
+				var elmThis = $(this);
+				if (this.nodeType == 3) {
+					var $this = $(this);
+					$this.replaceWith($this.text().replace(/(\S)/g, "<span>$&</span>"));
+				}
+				setElm.each(function () {
+					$(this)
+						.find("span")
+						.each(function (i) {
+							i = i + 1;
+							$(this).addClass(className + i);
+						});
+				});
+			});
+	}
 
-$(window).on('load',function(){
-  // ここから文字を<span></span>で囲む記述
-  for (var i = 0; i <= 11; i++){
-    var setElm = $('.txt'+i),className = 't'+i+'-';
-    setElm.children().addBack().contents().each(function(){
-      var elmThis = $(this);
-      if (this.nodeType == 3) {
-          var $this = $(this);
-          $this.replaceWith($this.text().replace(/(\S)/g, '<span>$&</span>'));
-      }
-      setElm.each(function(){
-          $(this).find('span').each(function(i){
-              i = i+1;
-              $(this).addClass(className+i);
-          });
-      });
-    });
-  }
-
-  // ここから一文字ずつフェードインさせる記述
-  setTimeout(function(){
-    $('.txt').css({'opacity':1});
-    for (var i = 0; i <= $('.txt').children().size(); i++) {
-      $('.txt').children('span:eq('+i+')').delay(50*i).animate({'opacity':1},50);
-    };
-  },500);
+	// ここから一文字ずつフェードインさせる記述
+	setTimeout(function () {
+		$(".txt").css({ opacity: 1 });
+		for (var i = 0; i <= $(".txt").children().size(); i++) {
+			$(".txt")
+				.children("span:eq(" + i + ")")
+				.delay(50 * i)
+				.animate({ opacity: 1 }, 50);
+		}
+	}, 500);
 });
 
 // 文字フェードアウト
-$(function(){
-  setTimeout(function(){
-      $('.txt').animate({'opacity':0},200);
-  },17500);
+$(function () {
+	setTimeout(function () {
+		$(".txt").animate({ opacity: 0 }, 200);
+	}, 17500);
 });
 
-
 // ロゴフェードイン
-$(function(){
-  setTimeout(function(){
-      $('.image').animate({'opacity':1},150);
-  },18500);
+$(function () {
+	setTimeout(function () {
+		$(".image").animate({ opacity: 1 }, 150);
+	}, 18000);
 });
 
 // 背景カラーチェンジ & ロゴフェードアウト & 顔アイコン登場
-$(function(){
-  setTimeout(function(){
-      $('body').animate({'backgroundColor':'#82CBCA'},200);
-      $('.image').animate({'opacity':0},200);
-      $('.txt-wrap').css({'display': 'none'});
-      $('.image-wrap').css({'display': 'flex'});
-  },19500);
+$(function () {
+	setTimeout(function () {
+		$("body").animate({ backgroundColor: "#82CBCA" }, 200);
+		$(".image").animate({ opacity: 0 }, 200);
+		$(".txt-wrap").css({ display: "none" });
+		$(".image-wrap").css({ display: "flex" });
+	}, 19500);
 });
