2cba6658a9d102c84d9bde23e6711ed18aa5bc29
sawasick <50393229+sawasick@users.noreply.github.com>
2020-09-29 02:45:35 +0900

    update
    add image message
    
    complete all

--- js/main.js
+++ js/main.js
@@ -1,108 +1,3 @@
-// var i=0;
-// var printString;
-// var txt1 ='@import';
-// var txt2 = '"co:De";';
-// var txt3 = 'var ';
-// var txt4 = 'thanks ';
-// var txt5 = '= ';
-// var txt6 = '"from 19 members to 4 you."';
-
-// function printTxt1(callback) {
-//     document.getElementById("op-text1").innerText=txt1.substring(0,i++);
-//     if(i<=txt1.length) {
-//     setTimeout("printTxt1()",100);
-//     }
-//     callback();
-// }
-
-// var printTxt2 = function(callback){
-//     document.getElementById("op-text2").innerText=txt2.substring(0,i++);
-//     if(i<=txt2.length) {
-//      setTimeout("printTxt2()",100);
-//     }
-//     callback();
-// }
-
-// var printTxt3 = function(callback){
-//     document.getElementById("op-text3").innerText=txt3.substring(0,i++);
-//     if(i<=txt3.length) {
-//      setTimeout("printTxt3()",100);
-//     }
-//     callback();
-// }
-
-// window.onload = printTxt1(printTxt2);
-// printTxt2(printTxt3);
-
-// 上から順番に処理を実行する
-// Promise.resolve()
-//   .then(printTxt1) // .then(実行したい関数名)
-//   .then(printTxt2)
-//   .then(printTxt3)
-//   .then(function (value){ // 直に処理を直接書いてもOK
-//     return new Promise(function (resolve, reject) {
-//       console.log(value);
-//       console.log("end.");
-//       resolve();
-//     });
-//   });
-
-// function printTxt1(){
-//   var ok = false;
-//   return new Promise(function (resolve, reject) {
-//     console.log("//////");
-//     setTimeout(function() {
-//       document.getElementById("op-text1").innerText=txt1.substring(0,i++);
-//       console.log("txt1.length is "+txt1.length);
-//       if(i<=txt1.length) {
-//         console.log(i);
-//         setTimeout("printTxt1()",100);
-//       }
-//       if(i == txt1.length) {
-//         console.log("111111111");
-//         console.log("aaaaaa");
-//         ok = true;
-//       }
-//       if(ok){
-//         console.log("go");
-//         resolve('printTxt1 ok');
-//       }
-//     });
-//   });
-// }
-
-// function printTxt2(value){
-//   return new Promise(function (resolve, reject) {
-//     setTimeout(function() {
-//         console.log(value);
-//         document.getElementById("op-text2").innerText=txt2.substring(0,i++);
-//         if(i<=txt2.length) {
-//           setTimeout("printTxt2()",100);
-//         }
-//         if(i == txt2.length) {
-//           console.log("22222222222");
-//         }
-//         resolve('printTxt2 ok');
-//     });
-//   });
-// }
-
-// function printTxt3(value){
-//   return new Promise(function (resolve, reject) {
-//     setTimeout(function() {
-//       console.log(value);
-//       document.getElementById("op-text3").innerText=txt3.substring(0,i++);
-//       if(i<=txt3.length) {
-//         setTimeout("printTxt3()",100);
-//       }
-//       if(i == txt3.length) {
-//         console.log("333333333");
-//         resolve('printTxt3 ok');
-//       }
-//     });
-//   });
-// }
-
 $(window).on("load", function () {
 	// ここから文字を<span></span>で囲む記述
 	for (var i = 0; i <= 11; i++) {
@@ -161,16 +56,22 @@ $(function () {
 		$("body").animate({ backgroundColor: "#82CBCA" }, 200);
 		$(".image").animate({ opacity: 0 }, 200);
 		$(".txt-wrap").css({ display: "none" });
-		$(".image-wrap").css({ display: "flex" });
+		$(".member-wrap").css({ display: "flex" });
 	}, 19500);
 });
 
-//モーダル
+// ------モーダル-----
 $(function () {
-	$(".modalBtn-open").on("click", function () {
-		$(".modal-wrap").fadeIn();
-		return false;
+	$(".modalBtn-open").each(function () {
+		$(this).on("click", function () {
+			var target = $(this).data("target");
+			var modal = document.getElementById(target);
+
+			$(modal).fadeIn();
+			return false;
+		});
 	});
+
 	$(".modalBtn-close").on("click", function () {
 		$(".modal-wrap").fadeOut();
 		return false;
